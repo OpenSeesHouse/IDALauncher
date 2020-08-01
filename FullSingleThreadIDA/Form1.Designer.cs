80b4ae922d43354c0df8cd1966a0d9a47ba92322
Seyed Alireza Jalali <seyedalirezajalali@gmail.com>
2020-08-02 02:10:41 +0430

    Updated GUI output for Full Single-Threaded IDA

--- FullSingleThreadIDA/Form1.Designer.cs
+++ FullSingleThreadIDA/Form1.Designer.cs
@@ -28,140 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.SuspendLayout();
             // 
-            // label5
+            // tabControl1
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(23, 15);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Running:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(112, 15);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(54, 17);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "Record";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(322, 15);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(22, 17);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "IM";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(112, 39);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(23, 17);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "---";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(323, 39);
-            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(23, 17);
-            this.label9.TabIndex = 13;
-            this.label9.Text = "---";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(225, 15);
-            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(48, 17);
-            this.label10.TabIndex = 14;
-            this.label10.Text = "#Point";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(235, 39);
-            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(16, 17);
-            this.label11.TabIndex = 15;
-            this.label11.Text = "1";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(543, 39);
-            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(38, 17);
-            this.label12.TabIndex = 17;
-            this.label12.Text = "Hunt";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(545, 15);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(45, 17);
-            this.label13.TabIndex = 16;
-            this.label13.Text = "Stage";
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 12);
+            this.tabControl1.MinimumSize = new System.Drawing.Size(433, 199);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(433, 199);
+            this.tabControl1.TabIndex = 18;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(615, 107);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(434, 211);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Hunt-Fill IDA";
             this.Activated += new System.EventHandler(this.Form1_Activated);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TabControl tabControl1;
     }
 }
 
--- FullSingleThreadIDA/Form1.cs
+++ FullSingleThreadIDA/Form1.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,11 @@ namespace IDALauncher
 {
     public partial class Form1 : Form
     {
+        private delegate void showResultsDelegate(int iRec, int nPnt, double sa, double disp, double slope,
+            double slopeRat, int clpsFlag, int failureFlag, double endTime, string stage);
+
+        private delegate void addRecordTabDelegate(int rec);
+
         double SaStart;
         double SaStep;
         double SaStepIncr;
@@ -23,9 +29,12 @@ namespace IDALauncher
         string TclModelPath;
         string OpsExePath;
         bool GoFill;
+        Dictionary<int,DataGridView> DGWs = new Dictionary<int, DataGridView>();
         public Form1(double saStart, double saStep, double saStepIncr, int maxNumPoints, bool goFill, List<int> recList, string tclPath, string opsPath)
         {
+
             InitializeComponent();
+
             SaStart = saStart;
             SaStep = saStep;
             SaStepIncr = saStepIncr;
@@ -36,6 +45,115 @@ namespace IDALauncher
             GoFill = goFill;
         }
 
+        private void addRecordTab(int rec)
+        {
+            if (InvokeRequired)
+            {
+                var d = new addRecordTabDelegate(addRecordTab);
+                Invoke(d, new object[] { rec });
+            }
+            else
+            {
+                tabControl1.SuspendLayout();
+                this.SuspendLayout();
+
+                var tp = new TabPage();
+                tp.SuspendLayout();
+                tp.Location = new Point(4, 22);
+                tp.Name = $"tp{rec}";
+                tp.Padding = new Padding(3);
+                tp.Size = new Size(455, 204);
+                tp.TabIndex = 0;
+                tp.Text = $"Rec-{rec}";
+                tp.UseVisualStyleBackColor = true;
+                tabControl1.Controls.Add(tp);
+
+                var dgw = new DataGridView();
+                ((ISupportInitialize)(dgw)).BeginInit();
+                tp.Controls.Add(dgw);
+                dgw.AllowUserToAddRows = false;
+                dgw.AllowUserToDeleteRows = false;
+                dgw.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+                dgw.Dock = DockStyle.Fill;
+                dgw.Location = new Point(3, 3);
+                dgw.Name = $"DGW{rec}";
+                dgw.ReadOnly = true;
+                dgw.Size = new Size(449, 198);
+                dgw.TabIndex = 0;
+
+                var clmn1 = new DataGridViewTextBoxColumn();
+                clmn1.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn1.Frozen = true;
+                clmn1.HeaderText = "Num";
+                clmn1.Name = "Num";
+                clmn1.ReadOnly = true;
+                clmn1.Width = 25;
+
+                var clmn2 = new DataGridViewTextBoxColumn();
+                clmn2.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn2.Frozen = false;
+                clmn2.HeaderText = "IM";
+                clmn2.Name = "IM";
+                clmn2.ReadOnly = true;
+                clmn2.Width = 75;
+
+                var clmn3 = new DataGridViewTextBoxColumn();
+                clmn3.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn3.Frozen = false;
+                clmn3.HeaderText = "SlopeRat";
+                clmn3.Name = "SR";
+                clmn3.ReadOnly = true;
+                clmn3.Width = 75;
+
+                var clmn4 = new DataGridViewTextBoxColumn();
+                clmn4.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn4.Frozen = false;
+                clmn4.HeaderText = "Collapsed";
+                clmn4.Name = "Clps";
+                clmn4.ReadOnly = true;
+                clmn4.Width = 40;
+
+                var clmn5 = new DataGridViewTextBoxColumn();
+                clmn5.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn5.Frozen = false;
+                clmn5.HeaderText = "Diverged";
+                clmn5.Name = "Divrgd";
+                clmn5.ReadOnly = true;
+                clmn5.Width = 40;
+
+                var clmn6 = new DataGridViewTextBoxColumn();
+                clmn6.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn6.Frozen = false;
+                clmn6.HeaderText = "EndTime";
+                clmn6.Name = "EndT";
+                clmn6.ReadOnly = true;
+                clmn6.Width = 40;
+
+                var clmn7 = new DataGridViewTextBoxColumn();
+                clmn7.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn7.Frozen = false;
+                clmn7.HeaderText = "Stage";
+                clmn7.Name = "Stage";
+                clmn7.ReadOnly = true;
+                clmn7.Width = 40;
+
+                var clmn8 = new DataGridViewTextBoxColumn();
+                clmn8.AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+                clmn8.Frozen = false;
+                clmn8.HeaderText = "EDP";
+                clmn8.Name = "EDP";
+                clmn8.ReadOnly = true;
+                clmn8.Width = 75;
+
+                //num, sa, disp, slopeRat, clpsFlag, failureFlag, endTime, stage
+                dgw.Columns.AddRange(new DataGridViewColumn[] { clmn1, clmn2, clmn8, clmn3, clmn4, clmn5, clmn6, clmn7 });
+                ((ISupportInitialize)(dgw)).EndInit();
+
+                tp.ResumeLayout(false);
+                DGWs[rec] = dgw;
+                ResumeLayout(false);
+            }
+        }
         internal void startTrace()
         {
             var watch = Stopwatch.StartNew();
@@ -73,14 +191,11 @@ namespace IDALauncher
                 var n1 = 0;
                 var failureFlag = 0;
                 var initSlope = 0.0;
+
+                addRecordTab(iRec);
                 while (true)
                 {
                     var endTime = 0.0;
-                    label8.Text = string.Format(CultureInfo.InvariantCulture, "{0}", iRec);
-                    label9.Text = string.Format(CultureInfo.InvariantCulture, "{0}", sa);
-                    label11.Text = string.Format(CultureInfo.InvariantCulture, "{0}", nPnt+1);
-                    label12.Text = stage;
-                    this.Refresh();
                     var disp = 0.0;
                     var res = RunOps(iRec, sa, wdir, tclModelName, ref failureFlag, ref disp, ref endTime);
                     if (!res)
@@ -105,6 +220,7 @@ namespace IDALauncher
                     }
                     var line = string.Format(CultureInfo.InvariantCulture, "{0} {1} {2} {3} {4} {5} {6} {7}",
                         sa, disp, slope, slopeRat, clpsFlag, failureFlag, endTime, stage);
+                    showResults(iRec, nPnt, sa, disp, slope, slopeRat, clpsFlag, failureFlag, endTime, stage);
                     idaOut2.WriteLine(line);
                     idaOut2.Flush();
                     lineVec.Add(line);
@@ -164,7 +280,21 @@ namespace IDALauncher
             Console.WriteLine("IDA ended at: {0}", DateTime.Now);
             Console.WriteLine("Total IDA time = {0}", watch.Elapsed);
 
-            label5.Text = "Finished";
+        }
+
+        private void showResults(int iRec, int nPnt, double sa, double disp, double slope, double slopeRat, int clpsFlag, int failureFlag, double endTime, string stage)
+        {
+            //num, sa, disp, slopeRat, clpsFlag, failureFlag, endTime, stage
+            var dgw = DGWs[iRec];
+            if (dgw.InvokeRequired)
+            {
+                var d = new showResultsDelegate(showResults);
+                dgw.Invoke(d, new object[] {iRec, nPnt, sa, disp, slope, slopeRat, clpsFlag, failureFlag, endTime, stage });
+            }
+            else
+            {
+                dgw.Rows.Add(nPnt, sa, disp, slopeRat, clpsFlag, failureFlag, endTime, stage);
+            }
         }
 
         private void SortArray(ref List<string> lineVec, ref List<double> saVec)
@@ -300,7 +430,9 @@ namespace IDALauncher
 
         private void Form1_Activated(object sender, EventArgs e)
         {
-            startTrace();
+            Thread th = new Thread(new ThreadStart(startTrace));
+            th.Start();
+            Thread.Sleep(1000);
         }
     }
 }
